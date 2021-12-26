dd84fce678c8633372e2f4886797dfe01a0a55ca
Davison Matanga <42201154+dmatangany@users.noreply.github.com>
2021-12-26 18:55:54 +0200

    Update SpringBootRestImplApplication.java

--- src/main/java/be/g00glen00b/SpringBootRestImplApplication.java
+++ src/main/java/be/g00glen00b/SpringBootRestImplApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootRestImplApplication {
 
     public static void main(String[] args) {
-        
         SpringApplication.run(SpringBootRestImplApplication.class, args);
     }
 }
